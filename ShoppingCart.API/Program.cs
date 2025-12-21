0e167091a1cd9951e62d81669b9d4c263cff7104
Samiksha Kumari <kmri.samiksha@outlook.com>
2025-12-21 22:04:18 +0100

    Added Json serialization to enter ProductType by name

--- ShoppingCart.API/Program.cs
+++ ShoppingCart.API/Program.cs
@@ -9,7 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(
+            new System.Text.Json.Serialization.JsonStringEnumConverter());
+    }); 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
